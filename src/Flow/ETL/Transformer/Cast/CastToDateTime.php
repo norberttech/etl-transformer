e4d930c71a68532f524605636d478382667b5a80
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-05-21 11:54:56 +0200

    Added possibility to set datetime tz and convert to different tz

--- src/Flow/ETL/Transformer/Cast/CastToDateTime.php
+++ src/Flow/ETL/Transformer/Cast/CastToDateTime.php
@@ -8,8 +8,27 @@ use Flow\ETL\Row\Entry\DateTimeEntry;
 
 final class CastToDateTime extends CastEntry
 {
-    public function __construct(string $entryName, string $format)
+    public function __construct(string $entryName, string $format, ?string $timeZone = null, ?string $toTimeZone = null)
     {
-        parent::__construct($entryName, DateTimeEntry::class, [$format], fn (string $value) : \DateTimeImmutable => new \DateTimeImmutable($value));
+        parent::__construct(
+            $entryName,
+            DateTimeEntry::class,
+            [$format],
+            function (string $dateTimeString) use ($timeZone, $toTimeZone) : \DateTimeImmutable {
+                if ($timeZone && $toTimeZone) {
+                    return (new \DateTimeImmutable($dateTimeString, new \DateTimeZone($timeZone)))->setTimezone(new \DateTimeZone($toTimeZone));
+                }
+
+                if ($timeZone) {
+                    return new \DateTimeImmutable($dateTimeString, new \DateTimeZone($timeZone));
+                }
+
+                if ($toTimeZone) {
+                    return (new \DateTimeImmutable($dateTimeString))->setTimezone(new \DateTimeZone($toTimeZone));
+                }
+
+                return new \DateTimeImmutable($dateTimeString);
+            }
+        );
     }
 }
