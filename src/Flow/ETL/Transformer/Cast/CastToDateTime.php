07dac8916b41e1b7de4d94c6ede9d9bcb0dae102
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-05-21 12:01:35 +0200

    Added documentation explaining different between  and

--- src/Flow/ETL/Transformer/Cast/CastToDateTime.php
+++ src/Flow/ETL/Transformer/Cast/CastToDateTime.php
@@ -8,6 +8,22 @@ use Flow\ETL\Row\Entry\DateTimeEntry;
 
 final class CastToDateTime extends CastEntry
 {
+    /**
+     * $timezone - this value should be used for datetime values that does not come with explicit tz to avoid using system default.
+     * For example when the datetime is "2020-01-01 00:00:00" and we know that it's utc, then $timeZone should be set to 'UTC'.
+     *
+     * $toTimeZone - this value should be used to convert datetime to different timezone. So when the datetime comes in one timezone
+     * "2020-01-01 00:00:00 UTC" and we want to convert it to America/Los_Angeles use $toTimeZone = 'America/Los_Angeles".
+     * If datetime comes without origin timezone, like for example '2020-01-01 00:00:00' but we know it's UTC
+     * and we want to cast it to 'America/Los_Angeles' use $timeZone = 'UTC' and $toTimeZone = 'America/Los_Angeles'.
+     *
+     * @param string $entryName
+     * @param string $format
+     * @param null|string $timeZone
+     * @param null|string $toTimeZone
+     *
+     * @throws \Flow\ETL\Exception\InvalidArgumentException
+     */
     public function __construct(string $entryName, string $format, ?string $timeZone = null, ?string $toTimeZone = null)
     {
         parent::__construct(
