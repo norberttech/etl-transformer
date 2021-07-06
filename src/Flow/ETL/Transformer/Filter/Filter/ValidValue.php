98dddaffbf3fec68e25242776de9793ffe2b98e3
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-07-06 10:24:40 +0200

    Suppress psalm error in Filter ValidValue

--- src/Flow/ETL/Transformer/Filter/Filter/ValidValue.php
+++ src/Flow/ETL/Transformer/Filter/Filter/ValidValue.php
@@ -25,6 +25,7 @@ final class ValidValue implements Filter
 
     public function keep(Row $row) : bool
     {
+        /** @psalm-suppress ImpureMethodCall */
         return $this->validator->isValid($row->valueOf($this->entryName));
     }
 }
